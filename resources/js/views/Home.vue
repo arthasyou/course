49f9f997134c7418efc6cabbc11925da27128d49
ysx <arthas.you@163.com>
2020-04-04 10:03:19 +0630

    hjgijgog

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,3 +1,6 @@
 <template>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quibusdam animi, sint accusamus magnam vero aut molestias recusandae doloremque. Assumenda voluptatum, a aut cumque? Odio corporis magni accusantium qui rem.</p>
+	<v-card>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit quibusdam animi, sint accusamus magnam vero aut molestias recusandae doloremque. Assumenda voluptatum, a aut cumque? Odio corporis magni accusantium qui rem.</p>
+	</v-card>
+	
 </template>
